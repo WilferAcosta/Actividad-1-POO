3763e6b3fd8cb04078975d8df8783c900ce6c4b6
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-29 14:20:32 -0500

    feat: :art: ordenando
    ordenando todo con mudulos

--- actividad-1/estudiante.js
+++ actividad-1/estudiante.js
@@ -1,7 +1,7 @@
 import { Persona } from "./persona.js";
 export class Estudiante extends Persona{
     #carrera
-    constructor(nombre,edad,genero,carrera){
+    constructor(nombre, edad, genero, carrera){
         //uso del super para heredar sus atributos de la clase padre
         super(nombre,edad,genero);
         this.#carrera = carrera;
--- actividad-1/main.js
+++ actividad-1/main.js
@@ -1,5 +1,5 @@
 import { Persona } from "./persona.js";
-import { Estudiante } from "./estudiante";
+import { Estudiante } from "./estudiante.js";
 
 const boton = document.querySelector('#sumit');
 boton.addEventListener('click', e =>{
@@ -9,15 +9,15 @@ boton.addEventListener('click', e =>{
     let genero = document.getElementById("genero").value;
     let carrera = document.getElementById("carrera").value;
     let estudiante1 = new Estudiante(nombre,edad,genero,carrera);
-    localStorage.setItem('estudiante', JSON.stringify(estudiante1));
-    let estudianteGuardado = localStorage.getItem('estudiante');
-    if (estudianteGuardado) {
-        let estudianteRecuperado = JSON.parse(estudianteGuardado);
-        document.getElementById("nombre").value = estudianteRecuperado.getNombre();
-        document.getElementById("edad").value = estudianteRecuperado.getEdad();
-        document.getElementById("genero").value = estudianteRecuperado.getGenero();
-        document.getElementById("carrera").value = estudianteRecuperado.getCarrera();
-    }
+    // localStorage.setItem('estudiante', JSON.stringify(estudiante1));
+    // let estudianteGuardado = localStorage.getItem('estudiante');
+    // if (estudianteGuardado) {
+    //     let estudianteRecuperado = JSON.parse(estudianteGuardado);
+    //     document.getElementById("nombre").value = estudianteRecuperado.getNombre();
+    //     document.getElementById("edad").value = estudianteRecuperado.getEdad();
+    //     document.getElementById("genero").value = estudianteRecuperado.getGenero();
+    //     document.getElementById("carrera").value = estudianteRecuperado.getCarrera();
+    // }
     alert("El estudiante es mayor de edad :  "+Persona.esMayorDeEdad(estudiante1.getEdad));
     alert(estudiante1.saludar());
     alert(estudiante1.estudiar());
