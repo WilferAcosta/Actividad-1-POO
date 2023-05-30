1285556aacb623912cc3a18a06b14dd068ddcc31
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-29 21:28:29 -0500

    feat: :construction: captura de datos
    captura de datos con el Object.fromEntries(new FormData(e.target));

--- actividad-1/main.js
+++ actividad-1/main.js
@@ -1,14 +1,22 @@
 import { Persona } from "./persona.js";
 import { Estudiante } from "./estudiante.js";
-
-const boton = document.querySelector('#sumit');
-boton.addEventListener('click', e =>{
+let info = new Array;
+const boton = document.querySelector('form');
+boton.addEventListener('submit', e =>{
     e.preventDefault();
-    let nombre = document.getElementById("nombre").value;
-    let edad = document.getElementById("edad").value;
-    let genero = document.getElementById("genero").value;
-    let carrera = document.getElementById("carrera").value;
-    let estudiante1 = new Estudiante(nombre,edad,genero,carrera);
+    const data = Object.fromEntries(new FormData(e.target));
+    info.push(data);
+    const estudiante1 = new Estudiante(data.nombre,data.edad,data.genero,data.carrera);
+    alert("El estudiante es mayor de edad :  "+Persona.esMayorDeEdad(estudiante1.getEdad));
+    alert(estudiante1.saludar());
+    alert(estudiante1.estudiar());
+    document.querySelector('form').reset();
+});
+    //let nombre = document.getElementById("nombre").value;
+    //let edad = document.getElementById("edad").value;
+    //let genero = document.getElementById("genero").value;
+    //let carrera = document.getElementById("carrera").value;
+    //let estudiante1 = new Estudiante(nombre,edad,genero,carrera);
     // localStorage.setItem('estudiante', JSON.stringify(estudiante1));
     // let estudianteGuardado = localStorage.getItem('estudiante');
     // if (estudianteGuardado) {
@@ -18,7 +26,4 @@ boton.addEventListener('click', e =>{
     //     document.getElementById("genero").value = estudianteRecuperado.getGenero();
     //     document.getElementById("carrera").value = estudianteRecuperado.getCarrera();
     // }
-    alert("El estudiante es mayor de edad :  "+Persona.esMayorDeEdad(estudiante1.getEdad));
-    alert(estudiante1.saludar());
-    alert(estudiante1.estudiar());
-});
+    
